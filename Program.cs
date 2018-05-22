72ab7ab97cc55851195feb8fd01488041385fd12
Aaron Bockover <abock@microsoft.com>
2018-05-22 11:49:41 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using YamlDotNet.Serialization;
+
+namespace YamlDotNet_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                new SerializerBuilder().Build().Serialize(
+                  new DeserializerBuilder().Build().Deserialize<Dictionary<string, string>>(
+                      "key1: value1\nkey2: value2")));
+        }
+    }
+}
